bb7eb2bf726f461c06d1335f0943cd329c1cbd39
Willbemax <max22042007@yandex.ru>
2024-09-23 21:33:15 +0300

    fix login page

--- src/2_page/auth/LoginPage/ui/LoginPage.tsx
+++ src/2_page/auth/LoginPage/ui/LoginPage.tsx
@@ -1,17 +1,40 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useLayoutEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { Typography } from 'antd'
+import { useRouter } from 'next/navigation'
 
 import { LoginFormWidget } from '@/3_widgets/auth/Login'
 import { removeLoading } from '@/5_entities/app/model/slice'
+import { useFetchUser } from '@/5_entities/user/lib/hooks/useFetchUser'
 import { Container } from '@/6_shared/ui/continer'
 
 import classes from './classes.module.scss'
 
 const LoginPage: React.FC = () => {
   const dispatch = useDispatch()
+  const fetchUser = useFetchUser()
+  const router = useRouter()
+
+  useLayoutEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetchUser()
+
+        if (res.payload === 500) throw new Error('Ошибка входа')
+        router.push('/')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    const timer = setTimeout(() => {
+      checkAuth()
+    }, 0)
+
+    return () => clearTimeout(timer)
+  }, [dispatch, router])
 
   useEffect(() => {
     dispatch(removeLoading())
